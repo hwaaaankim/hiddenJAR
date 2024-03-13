88cc5a3a45b984d1ab1c66cd7818f607ec9924db
thramania <thramania@hanmail.net>
2024-03-13 17:42:05 +0900

    jar init

---
+++ src/main/java/com/dev/HiddenBATHJar/controller/SurveyController.java
@@ -0,0 +1,22 @@
+package com.dev.HiddenBATHJar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/survey")
+public class SurveyController {
+
+	@GetMapping({"", "/"})
+	public String surveyHome() {
+		
+		return "front/survey/index";
+	}
+	
+	@GetMapping("/information")
+	public String surveyInformation() {
+		
+		return "front/survey/information";
+	}
+}
---
+++ src/test/java/com/dev/HiddenBATHJar/HiddenBathJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.HiddenBATHJar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenBathJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
