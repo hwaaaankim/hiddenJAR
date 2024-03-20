07edd2cf535dca3767f0cf8660714aeeff84075a
hwaaaankim <thramania@hanmail.net>
2024-03-20 18:25:03 +0900

    fixing order

--- src/main/resources/static/front/order/js/wizard_func_multiple_branch.js
+++ src/main/resources/static/front/order/js/wizard_func_multiple_branch.js
@@ -12,6 +12,7 @@
 			stepsWrapper: "#wrapped",
 			submit: ".submit",
 			beforeSelect: function (event, state) {
+				console.log(state);
 				if ($('input#website').val().length != 0) {
 					return false;
 				}
@@ -22,7 +23,57 @@
 			},
 			afterSelect:function(event, state){
 				if(state.stepIndex === 13){
+					console.log('click');
 					if($('input[name=model]:checked').attr('id')!=undefined){
+						$('#mirrorShape').val('거울 모델 : ' + $('input[name=model]').attr('id'));
+					}
+					
+					if($('input[name=frame]:checked').attr('id')==='프레임있음[Yes]'){
+						$('#mirrorFrame').val('프레임 여부 : ' + $('input[name=frame]').attr('id'));
+						$('#mirrorStyle').val('프레임 스타일 : ' + $('input[name=style]:checked').attr('id'));
+						$('#mirrorColor').val('프레임 색상 : ' + $('input[name=color]:checked').attr('id'));
+					}else{
+						$('#mirrorFrame').val('프레임 여부 : ' + $('input[name=frame]').attr('id'));
+						$('#mirrorStyle').val('프레임 스타일 : 해당 없음');
+						$('#mirrorColor').val('프레임 색상 : 해당 없음');
+					}
+					
+					$('#mirrorWidth').val('가로길이 : ' + $('input[id=sizeWidth]').val() + "[단위 : mm]");
+					$('#mirrorHeight').val('세로길이 : ' + $('input[id=sizeHeight]').val() + "[단위 : mm]");
+					
+					if($('#sizeMessage').val()!=undefined){
+						$('#mirrorMessage').val('남김말 : ' + $('#sizeMessage').val());
+					}else{
+						$('#mirrorMessage').val('남김말 : 없음');
+					}
+					
+					if($('input[name=led]:checked').attr('id') === 'LED추가함[YES]'){
+						$('#mirrorLed').val('LED 추가여부 : ' + $('input[name=led]:checked').attr('id'));
+						$('#mirrorLedMethod').val('LED 동작방식 : ' + $('input[name=method]:checked').attr('id'));
+						if($('input[name=method]:checked').attr('id') === '터치식LED[Touchable]'){
+							$('#mirrorLedForm').val('LED 형태 : ' + $('input[name=form]:checked').attr('id'));
+							$('#mirrorSensor').val('센서 추가여부 : 해당없음');
+							$('#mirrorSensorForm').val('센서 형태 : 해당없음');
+						}else{
+							$('#mirrorLedForm').val('LED 형태 : 해당없음');
+							$('#mirrorSensor').val('센서 추가여부 : ' + $('input[name=sensor]:checked').attr('id'));
+							if($('input[name=sensor]:checked').attr('id') === '모션감지센서추가함[Add]'){
+								$('#mirrorSensorForm').val('센서 형태 : ' + $('input[name=sensor_form]:checked').attr('id'));
+							}else{
+								$('#mirrorSensorForm').val('센서 형태 : 해당없음');
+							}
+						}
+						
+					}else{
+						$('#mirrorLed').val('LED 추가여부 : ' + $('input[name=led]:checked').attr('id'));
+						$('#mirrorLedMethod').val('LED 동작방식 : 해당없음');
+						$('#mirrorLedForm').val('LED 형태 : 해당없음');
+						$('#mirrorSensor').val('센서 추가여부 : 해당없음');
+						$('#mirrorSensorForm').val('센서 형태 : 해당없음');
+					}
+					
+					
+					/*if($('input[name=model]:checked').attr('id')!=undefined){
 						$('#mirrorShape').val('거울 모델 : ' + $('input[name=model]').attr('id'));
 					}
 					
@@ -45,8 +96,8 @@
 					$('#mirrorWidth').val('가로길이 : ' + $('input[id=sizeWidth]').val() + "[단위 : mm]");
 					$('#mirrorHeight').val('세로길이 : ' + $('input[id=sizeHeight]').val() + "[단위 : mm]");
 					
-					if($('input[id=sizeMessage]').val()!=undefined){
-						$('#mirrorMessage').val('남김말 : ' + $('input[id=sizeMessage]').val());
+					if($('#sizeMessage').val()!=undefined){
+						$('#mirrorMessage').val('남김말 : ' + $('#sizeMessage').val());
 					}else{
 						$('#mirrorMessage').val('남김말 : 없음');
 					}
@@ -77,7 +128,7 @@
 						$('#mirrorSensorForm').val('센서 형태 : ' + $('input[name=sensor_form]:checked').attr('id'));
 					}else{
 						$('#mirrorSensorForm').val('센서 형태 : 해당없음');
-					}
+					}*/
 				}
 			}
 		}).validate({
